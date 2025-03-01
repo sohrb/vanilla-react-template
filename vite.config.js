5421b4ecc9ffdfcd0d821a63f235df25040cdd52
Sohrab Chegini <sohrab.chegini@gmail.com>
2025-03-01 22:03:59 +0330

    setup typescript

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-const config = defineConfig({});
+const config = defineConfig({
+  plugins: [tsconfigPaths()],
+});
 
 export default config;
