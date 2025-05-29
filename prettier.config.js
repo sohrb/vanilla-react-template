c1910b50371c8383dabe4509294856678abe2fbe
Sohrab Chegini <sohrab.chegini@gmail.com>
2025-05-29 22:39:13 +0330

    setup react

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: ["prettier-plugin-packagejson"],
+  overrides: [
+    {
+      files: "*.svg",
+      options: {
+        parser: "html",
+      },
+    },
+  ],
 };
 
 export default config;
