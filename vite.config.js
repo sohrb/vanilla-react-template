5daf2ea230c7d920541c06d5f5ac50e220149058
Sohrab Chegini <sohrab.chegini@gmail.com>
2025-10-31 23:43:02 +0330

    setup react compiler

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
 });
 
 export default config;
